587ea3c6b3e1d389b216044e9523af89ac52399a
Richard Wang <rw22448@gmail.com>
2023-11-15 16:50:40 +1100

    feature: add types packages, being implementing index

---
+++ index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+const PERSONAL_API_KEY = process.env.PERSONAL_API_KEY || '';
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+axios.interceptors.request.use((req) => {
+  req.headers['X-Riot-Token'] = PERSONAL_API_KEY;
+  return req;
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
