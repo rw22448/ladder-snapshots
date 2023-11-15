4cefddb296ceda6b59cefda86704cee047743fc9
Richard Wang <rw22448@gmail.com>
2023-11-15 21:56:14 +1100

    wip: change pkg config

--- snapshot.ts
+++ snapshot.ts
@@ -1,8 +1,13 @@
 import axios from "axios"
+import fs from "fs"
+import path from "path"
+import { exit } from "process"
 
 require("dotenv").config()
 
 const PERSONAL_API_KEY = process.env.PERSONAL_API_KEY || ""
+const API_HOST = process.env.API_HOST || ""
+const DEFAULT_LOG_DELAY = 2000 // TODO: Set to 2000
 
 const delay = (milliseconds: number) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds))
@@ -10,7 +15,7 @@ const delay = (milliseconds: number) => {
 
 const logThenDelay = async (
   message: string,
-  delayMilliseconds: number = 2000
+  delayMilliseconds: number = DEFAULT_LOG_DELAY
 ) => {
   console.log(message)
   await delay(delayMilliseconds)
@@ -19,6 +24,24 @@ const logThenDelay = async (
 export const generateSnapshot = async () => {
   await logThenDelay("Running ladder-snapshot...")
   await logThenDelay("Generating snapshot...")
+
+  let data: string = ""
+  try {
+    logThenDelay(__dirname, 0)
+    logThenDelay(process.cwd(), 0)
+    data = fs.readFileSync(path.resolve(process.cwd(), "input.txt"), "utf-8")
+    // logThenDelay(data)
+  } catch {
+    console.error("Error: File 'input.txt' not found in expected position.")
+    exit(0)
+  }
+
+  let names: string[] = data.split(/\r?\n/)
+  names.forEach((name, index) => {
+    logThenDelay(`${index + 1}: ${name}`)
+  })
+
+  // fs.writeFileSync(path.resolve(process.cwd(), "output.txt"), data)
 }
 
 generateSnapshot()
